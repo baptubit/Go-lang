bc82b11ecf9b072e4485d5018155b11f25c5a109
subhankar <subhankar.das@nokia.com>
2019-05-22 01:14:04 +0530

    new file added and old file modified

--- Hello.go
+++ Hello.go
@@ -1,20 +1,20 @@
-package main
+//package main
 
-import "fmt"
+//import "fmt"
 
-func main() {
-	fmt.Println("Welcome to the world of GO-lang")
-	fmt.Println("I am in main")
-	foo()
-	fmt.Println("Lets do some iterative stuff")
+//func main() {
+	//fmt.Println("Welcome to the world of GO-lang")
+//	fmt.Println("I am in main")
+//	foo()
+//	fmt.Println("Lets do some iterative stuff")
 
-	for i := 0; i < 100; i++ {
-		if i%2 == 0 {
-			fmt.Println("The current loop=number is =", i)
-		}
+//	for i := 0; i < 100; i++ {
+//		if i%2 == 0 {
+	//		fmt.Println("The current loop=number is =", i)
+//		}
 
-	}
-}
+//	}
+//}
 
 func foo() {
 	fmt.Println("I am in foo")
