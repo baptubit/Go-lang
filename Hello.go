d2dc01b92f2eafaa15f7558fee3db5c6094654ab
subhankar <subhankar.das@nokia.com>
2019-05-21 05:40:25 +0530

    some new file

---
+++ Hello.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the world of GO-lang")
+	fmt.Println("I am in main")
+	foo()
+	fmt.Println("Lets do some iterative stuff")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("The current loop=number is =", i)
+		}
+
+	}
+}
+
+func foo() {
+	fmt.Println("I am in foo")
+}
+
+//Control flow:
+//1.Sequence
+//2.loop;iterative
+//3.Conditional
