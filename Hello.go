3863a47e6a61aab339f1cec53c943e66911565b7
subhankar <subhankar.das@nokia.com>
2019-05-22 02:26:08 +0530

    file modified 1

--- Hello.go
+++ Hello.go
@@ -7,8 +7,9 @@ func main() {
 	fmt.Println("I am in main")
 	foo()
 	fmt.Println("Lets do some iterative stuff")
-    FUNC2()
-	for i := 0; i < 100; i++ {
+	FUNC2()
+	DeclareVariable()
+	for i := 0; i < 10; i++ {
 		if i%2 == 0 {
 			fmt.Println("The current loop=number is =", i)
 		}
@@ -24,6 +25,13 @@ func FUNC2(){
 	n,err:=fmt.Println("Welcome to the world of GO-lang")
 	fmt.Println("Number of bytes printed=",n)
 	fmt.Println(err)	
+}
+
+func DeclareVariable(){
+	x:=45 //first time to declare variable we should use ":="
+	fmt.Println("Value of initilized Variable=",x)
+	x=90
+	fmt.Println("Changed value of x  is=",x)
 }
 
 //Control flow:
